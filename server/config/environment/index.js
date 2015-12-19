24c1df38a597d26d7669f6c28b19fbfdb08ae5c5
chinnichaitanya <chchaitanya95@gmail.com>
2015-12-19 19:41:53 +0530

    add roles

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -30,7 +30,7 @@ var all = {
   },
 
   // List of user roles
-  userRoles: ['guest', 'user', 'admin'],
+  userRoles: ['guest', 'user', 'worker', 'planner', 'admin'],
 
   // MongoDB connection options
   mongo: {
