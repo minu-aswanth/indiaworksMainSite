dc3275814b19056e448a0e2a58b17b09424ea62d
chinnichaitanya <chchaitanya95@gmail.com>
2016-03-09 19:04:32 +0530

    remove(thing model): remove `thing` model from the app

--- server/routes.js
+++ server/routes.js
@@ -13,7 +13,6 @@ module.exports = function(app) {
   app.use('/api/services', require('./api/service'));
   app.use('/api/subCategories', require('./api/subCategory'));
   app.use('/api/categories', require('./api/category'));
-  app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
